e97357ed75a9621c6b4d62a13218aac013e56238
Alex Varga <alexvarga09072001@gmail.com>
2021-11-03 11:46:35 +0200

    add all

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4.5,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+new Swiper(".mySwiper-mob", {
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+$(document).ready(function() {
+  $('.burger-menu').click(function() {
+      $('.burger-menu').toggleClass('open-menu');
+      $('.nav').toggleClass('open-menu');
+  });
+});
